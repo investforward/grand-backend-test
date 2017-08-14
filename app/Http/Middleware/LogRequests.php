f7e76c954c707f1efe3c11fd8dc7d046713299c8
Nichole Boseman <bnboseman@gmail.com>
2017-08-14 09:50:06 -0400

    added function to log request

---
+++ app/Http/Middleware/LogRequests.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\DB;
+
+class LogRequests
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        DB::table('logs')->insert(
+            ['route' => $request->path(),
+                'magical-header' => $request->headers->has('magical-header')]
+        );
+        return $next($request);
+    }
+
+}
